610bfb64765555765f7f207b2bcb803366e704a0
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2019-03-29 01:50:09 +1100

    Initial commit

---
+++ Assets/Scripts/SphereSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+    public GameObject spawner;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+    public int spawnLimit = 5;
+    public int currentSpawnCount = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+    public void SpawnObject()
+    {
+        Instantiate(spawner, transform.position, transform.rotation);
+        currentSpawnCount++;
+    }
+
+    private void Update()
+    {
+        
+        if (currentSpawnCount >= spawnLimit)
+        {
+            
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
